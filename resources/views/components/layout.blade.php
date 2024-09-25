312b7daf9580d1a1427828468fcb33e0e44b85f8
EtimbukUdofia <etimbukudofia9@gmail.com>
2024-09-25 16:06:21 +0100

    update and add components

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,10 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pixel Positions</title>
-    @vite(['resources/css/app.css','resources/js/app.js'])
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:wght@400;500;600&display=swap" rel="stylesheet">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class=" bg-black text-white">
+<body class=" bg-black text-white font-hanken-grotesk">
     <div class=" px-10">
         <nav class=" flex justify-between items-center py-4 border-b border-white/10">
             <div>
