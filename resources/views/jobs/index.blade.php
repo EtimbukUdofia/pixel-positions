b46d94f50cb8207a0217713e77b9f43a5b2d9c13
EtimbukUdofia <etimbukudofia9@gmail.com>
2024-09-27 11:41:40 +0100

    update index view

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -4,7 +4,8 @@
             <h1 class="font-bold text-4xl">Let's Find Your Next Job</h1>
 
             <form action="" class="mt-6">
-                <input type="text" placeholder="Web Developer..." class="rounded-xl bg-white/5 border-white/10 px-5 py-4 w-full max-w-xl">
+                <input type="text" placeholder="Web Developer..."
+                    class="rounded-xl bg-white/5 border-white/10 px-5 py-4 w-full max-w-xl">
             </form>
         </section>
 
@@ -12,9 +13,9 @@
             <x-section-heading>Featured Jobs</x-section-heading>
 
             <div class="grid lg:grid-cols-3 gap-8 mt-6">
-                <x-job-card />
-                <x-job-card />
-                <x-job-card />
+                @foreach ($featuredJobs as $job)
+                    <x-job-card :$job />
+                @endforeach
             </div>
         </section>
 
@@ -22,16 +23,9 @@
             <x-section-heading>Tags</x-section-heading>
 
             <div class="mt-6 space-x-1">
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
+                @foreach ($tags as $tag)
+                    <x-tag :tag='$tag' />
+                @endforeach
             </div>
         </section>
 
@@ -39,9 +33,9 @@
             <x-section-heading>Recent Jobs</x-section-heading>
 
             <div class="mt-6 space-y-6">
-                <x-job-card-wide />
-                <x-job-card-wide />
-                <x-job-card-wide />
+                @foreach ($jobs as $job)
+                    <x-job-card-wide :$job />
+                @endforeach
             </div>
         </section>
     </div>
