1c36b3fffa72ed174652741939ea08a9c85081fd
EtimbukUdofia <etimbukudofia9@gmail.com>
2024-09-29 21:35:36 +0100

    add middleware to routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,17 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [JobController::class, 'index']);
+
+Route::middleware('guest')->group(function(){
+  Route::get('/register', [RegisteredUserController::class, 'create']);
+  Route::post('/register', [RegisteredUserController::class, 'store']);
+  Route::get('/login', [SessionController::class, 'create']);
+  Route::post('/login', [SessionController::class, 'store']);
+});
+
+Route::delete('/logout', [SessionController::class, 'destory'])->middleware('auth');
