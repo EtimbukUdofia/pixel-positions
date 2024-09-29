374fea7950e486cc125a68e6cfe5413eec9ce37b
EtimbukUdofia <etimbukudofia9@gmail.com>
2024-09-29 21:20:38 +0100

    update layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -12,7 +12,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class=" bg-black text-white font-hanken-grotesk">
+<body class=" bg-black text-white font-hanken-grotesk pb-20">
     <div class=" px-10">
         <nav class=" flex justify-between items-center py-4 border-b border-white/10">
             <div>
