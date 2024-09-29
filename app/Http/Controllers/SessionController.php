aea64e4ae6a3f3bab5aafbcc1d107ce9a4291871
EtimbukUdofia <etimbukudofia9@gmail.com>
2024-09-29 21:23:45 +0100

    add controller to handle sessions

---
+++ app/Http/Controllers/SessionController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+class SessionController extends Controller
+{
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('auth.login');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $attributes = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+
+        if(! Auth::attempt($attributes)){
+            throw ValidationException::withMessages([
+                'email' => 'Sorry those credentials do not match.'
+            ]);
+        };
+
+        request()->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
