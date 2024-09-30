aeabc1f8b97b40b4be6a22cc3a6cccab6678edce
EtimbukUdofia <etimbukudofia9@gmail.com>
2024-09-30 13:48:21 +0100

    update components

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -8,7 +8,11 @@
     <div class="flex-1 flex flex-col">
         <a href="#" class=" self-start text-sm text-gray-500">{{$job->employer->name}}</a>
 
-        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 transition-colors duration-300">{{$job->title}}</h3>
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 transition-colors duration-300">
+            <a href="{{$job->url}}" target="_blank">
+                {{$job->title}}
+            </a>
+        </h3>
 
         <p class="text-sm text-gray-500 mt-auto">{{$job->schedule}} - From {{$job->salary}}</p>
     </div>
--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -4,7 +4,11 @@
     <div class=" self-start text-sm">{{ $job->employer->name }}</div>
 
     <div class="py-8">
-        <h3 class="group-hover:text-blue-800 text-xl font-bold transition-colors duration-300">{{ $job->title }}</h3>
+        <h3 class="group-hover:text-blue-800 text-xl font-bold transition-colors duration-300">
+            <a href="{{$job->url}}" target="_blank">
+                {{ $job->title }}
+            </a>
+        </h3>
         <p class="text-sm mt-4">{{ $job->schedule }} - From {{ $job->salary }}</p>
     </div>
 
