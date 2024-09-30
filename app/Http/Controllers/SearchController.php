7aaa3ec9bed4289dbbd5db10379d46459feafbac
EtimbukUdofia <etimbukudofia9@gmail.com>
2024-09-30 15:55:25 +0100

    optimize query to avoid n+1 problem

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -9,7 +9,7 @@ class SearchController extends Controller
 {
     public function __invoke()
     {
-        $jobs = Job::where('title', 'LIKE', '%'.request('q').'%')->get();
+        $jobs = Job::with(['employer', 'tags'])->where('title', 'LIKE', '%'.request('q').'%')->get();
 
         return view('results', [
             'jobs' => $jobs
