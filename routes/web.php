610c0377ea11c42d79a3ebd633b28e6d143e9a77
EtimbukUdofia <etimbukudofia9@gmail.com>
2024-09-30 13:47:44 +0100

    update routes

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,18 @@
 
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SearchController;
 use App\Http\Controllers\SessionController;
+use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [JobController::class, 'index']);
+
+Route::get('/jobs/create', [JobController::class, 'create'])->middleware('auth');
+Route::post('/jobs', [JobController::class, 'store'])->middleware('auth');
+
+Route::get('/search', SearchController::class);
+Route::get('/tags/{tag:name}', TagController::class);
 
 Route::middleware('guest')->group(function(){
   Route::get('/register', [RegisteredUserController::class, 'create']);
